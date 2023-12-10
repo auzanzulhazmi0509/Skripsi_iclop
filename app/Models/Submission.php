ea1e52b2bc2981f4b6202d6666757b11cf8aec81
auzanzulhazmi0509 <zulhazmiauzan@gmail.com>
2023-12-10 20:24:43 +0700

    Tanggal

--- app/Models/Submission.php
+++ app/Models/Submission.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
+use Carbon\Carbon;
 class Submission extends Model
 {
     protected $table = 'submissions';
@@ -12,11 +13,23 @@ class Submission extends Model
         'student_id',
         'question_id',
         'status',
-        'answer'
+        'answer',
+        'created_at',
+        'updated_at',
     ];
 
     public function soal()
     {
         return $this->belongsTo(Question::class, 'question_id', 'id');
+    }
+
+    public function getCreatedAtAttribute($value)
+    {
+        return Carbon::parse($value)->addHours(7); // Menambahkan 7 jam untuk mengubah ke WIB
+    }
+
+    public function getUpdatedAtAttribute($value)
+    {
+        return Carbon::parse($value)->addHours(7); // Menambahkan 7 jam untuk mengubah ke WIB
     }
 }
